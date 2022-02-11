d7845be2f23cc1a9360d4b2c3f9040e270cd5aa1
thelastandrew <thelastandrew@gmail.com>
2022-02-11 06:12:07 +0300

    feat: add change colo func for lang switcher

--- random-jokes/index.js
+++ random-jokes/index.js
@@ -2,6 +2,23 @@ const url = 'https://api.icndb.com/jokes/random';
 const btn = document.querySelector('.btn');
 const audio = document.querySelector('.audio');
 const quote = document.querySelector('.quote');
+const lang = document.querySelectorAll('.lang');
+let activeLang = document.querySelector('.active');
+
+//Lang switcher
+function changeColor(el) {
+  if (!el.classList.contains('active')) {
+    activeLang.classList.remove('active');
+    el.classList.add('active');
+    activeLang = el;
+  }
+}
+
+lang.forEach((element) => {
+  element.addEventListener('click', function (e) {
+    changeColor(element);
+  });
+});
 
 btn.addEventListener('click', (e) => {
   audio.play();
